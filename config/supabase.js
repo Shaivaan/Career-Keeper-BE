b795b8418acec46e7c4e2ea1dca47eb07387b6fb
Sivaan <smssmsvmas@gmail.com>
2026-05-16 13:59:05 +0530

    feat: add new package

--- config/supabase.js
+++ config/supabase.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
+const ws = require('ws');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    global: { fetch },
+    realtime: { transport: ws },
+  }
 );
 
 module.exports = { supabase };
